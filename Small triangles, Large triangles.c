2f3204e3fc8a7fb68353480a91ca5951c8aa80ba
Aditya Gupta <84505010+RockstarBuddies@users.noreply.github.com>
2024-10-31 04:02:44 +0530

    Create Small triangles, Large triangles.c

---
+++ Small triangles, Large triangles.c
@@ -0,0 +1,25 @@
+
+
+double area(triangle tr) {
+    double p = (tr.a + tr.b + tr.c) / 2.0;
+    return sqrt(p * (p - tr.a) * (p - tr.b) * (p - tr.c));
+}
+
+int compare(const void *a, const void *b) {
+    triangle *t1 = (triangle*) a;
+    triangle *t2 = (triangle*) b;
+    double area1 = area(*t1);
+    double area2 = area(*t2);
+    if (area1 > area2) {
+        return 1;
+    } else if (area1 < area2) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+void sort_by_area(triangle* tr, int n) {
+    qsort(tr, n, sizeof(triangle), compare);
+}
+
